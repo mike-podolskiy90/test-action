2e4b5d9c32047d107640bdadf429c31cafa160dd
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-25 22:26:48 +0200

    Try again

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,5 +10,6 @@ public class Test
   public static void main(String[] args) {
     System.out.println("Hello world");
     System.out.println("again");
+    System.out.println("and again");
   }
 }
