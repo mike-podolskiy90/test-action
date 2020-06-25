a26e193bca08c5a8ee627189e6bac4cef483c28a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-25 21:49:46 +0200

    Initial commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Set;
+import java.util.List;
+
+/**
+ * Test google format and github actions
+ */
+public class Test
+{
+  public static void main(String[] args) {
+    System.out.println(
+        "Hello world");
+  }
+}
