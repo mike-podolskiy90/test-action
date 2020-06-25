1575ee47abc7b1157b4c91e757d5ddba9b181cd6
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-25 23:44:37 +0200

    Test action

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,18 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+
 /**
  * Test google format and github actions
  */
 public class Test {
   public static void main(String[] args) {
+    Set<Object> objects = new HashSet<Object>();
+    List<Object> objects2 = new ArrayList<Object>();
     System.out.println("Hello world");
     System.out.println("again");
     System.out.println("and again");
