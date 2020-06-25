b30060b36c94afd366a2b0da09d089d70d828931
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-25 22:06:04 +0200

    Trying to spoil styles again

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,13 @@
 package test;
 
+import java.util.List;
 
-/** Test google format and github actions */
-public class Test {
+/**
+ * Test google format and github actions
+ */
+public class Test
+{
   public static void main(String[] args) {
     System.out.println("Hello world");
-    System.out.println("again");
   }
 }
